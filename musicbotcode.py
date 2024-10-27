b944c97acf0c6eebb17a02912107b7fb05014046
PabloViniegra <pablovpmadrid@gmail.com>
2024-10-27 16:27:58 +0100

    first commit

---
+++ musicbotcode.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+import asyncio
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+FFMPEG_OPTIONS = {'options': '-vn'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': True}
+
+class MusicBot(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.queue = []
+    
+    @commands.command()
+    async def play(self, ctx, *, search):
+        voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+        if not voice_channel:
+            return await ctx.send("No estás en un canal de voz empanado.")
+        if not ctx.voice_client:
+            await voice_channel.connect()
+            
+        async with ctx.typing():
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(f"ytsearch:{search}", download=False)
+                if 'entries' in info:
+                    info = info['entries'][0]
+                url = info['url']
+                title = info['title']
+                self.queue.append((url, title))
+                await ctx.send(f'Añadido a la cola: **{title}**')
+        
+        if not ctx.voice_client.is_playing():
+            await self.play_next(ctx)
+
+    async def play_next(self, ctx):
+        if self.queue:
+            url, title = self.queue.pop(0)
+            source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+            ctx.voice_client.play(source, after=lambda _:self.client.loop.create_task(self.play_next(ctx)))
+            await ctx.send(f'Ahora esta sonando: **{title}**')
+        elif not ctx.voice_client.is_playing():
+            await ctx.send("La cola está vacía bro")
+            
+    @commands.command()
+    async def skip(self, ctx):
+        if ctx.voice_client and ctx.voice_client.is_playing():
+            ctx.voice_client_stop()
+            await ctx.send("Omitido")
+            
+client = commands.Bot(command_prefix="/", intents=intents)
+
+async def main():
+    token = os.getenv('DISCORD_TOKEN')
+    if not token:
+        raise ValueError('No hay token de discord')
+    
+    await client.add_cog(MusicBot(client))
+    await client.start(token)
